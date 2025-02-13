3333329a8c534b8817ab52a23aff9ac71dc9dbd5
samonum <sam_mie_sam@hotmail.com>
2025-02-13 11:32:47 +0100

    Uploaded first sample

---
+++ 1. Fireworks 1/Fireworks/Fireworks/Firework.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Firework
+{
+	public Vector2 Position;
+	public Vector2 Velocity;
+	public Texture2D RocketSprite;
+	public Texture2D ExplosionSprite;
+	public SoundEffect Explosion;
+	public bool Exploded = false;
+	public double  ExplosionTimer = 2;
+	public Color FireworkColor;
+	public Vector2 Scale;
+
+	public Firework(Vector2 position, Vector2 velocity, Texture2D rocketSprite, Texture2D explosionSprite, SoundEffect explosion)
+	{
+		Position = position;
+		Velocity = velocity;
+		RocketSprite = rocketSprite;
+		ExplosionSprite = explosionSprite;	
+		Scale = new Vector2(2,2);
+		FireworkColor = Color.White;
+		Explosion = explosion;
+	}
+
+	public void Update(GameTime gametime)
+	{
+		if (!Exploded)
+		{
+			Position += Velocity;
+			if(Keyboard.GetState().IsKeyDown(Keys.Space))
+			{
+				Exploded = true;
+                Explosion.Play();
+            }
+		}
+		else if(ExplosionTimer >=0)
+		{
+			ExplosionTimer -= gametime.ElapsedGameTime.TotalSeconds;
+		}
+	}
+
+	public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+	{
+		if (!Exploded)
+		{
+			spriteBatch.Draw(RocketSprite, Position, null, Color.White, 0, RocketSprite.Bounds.Center.ToVector2(), Scale, SpriteEffects.None, 0);
+		}
+		else if (ExplosionTimer >= 0)
+        {
+            spriteBatch.Draw(ExplosionSprite, Position, null, FireworkColor, 0, ExplosionSprite.Bounds.Center.ToVector2(), Scale, SpriteEffects.None, 0);
+        }
+	}
+
+}
---
+++ 1. Fireworks 1/Fireworks/Fireworks/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Fireworks
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Firework _firework;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // Increase the size of the screen
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D rocketSprite = Content.Load<Texture2D>("Rocket");
+            Texture2D explosionSprite = Content.Load<Texture2D>("Fireworks");
+
+            // Source: https://freesound.org/people/theplax/sounds/560578/
+            SoundEffect explosion = Content.Load<SoundEffect>("Explosion");
+
+            Vector2 location = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2, _graphics.GraphicsDevice.Viewport.Height - 50);
+            Vector2 velocity = new Vector2(0, -5);
+
+
+            _firework = new Firework(location, velocity, rocketSprite, explosionSprite, explosion);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            _firework.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin();
+            _firework.Draw(gameTime, _spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
