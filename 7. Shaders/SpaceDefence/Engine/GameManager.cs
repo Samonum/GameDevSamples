a4a4116507e7ed33fc6164d409db495ce3be8d82
samonum <sam_mie_sam@hotmail.com>
2025-05-09 09:00:18 +0200

    Added shader sample code

---
+++ 7. Shaders/SpaceDefence/Engine/GameManager.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceDefence
+{
+    public class GameManager
+    {
+        private static GameManager gameManager;
+
+        private List<GameObject> _gameObjects;
+        private List<GameObject> _toBeRemoved;
+        private List<GameObject> _toBeAdded;
+        private ContentManager _content;
+        private Effect _teamColorEffect;
+        private Effect _fade;
+        public Matrix WorldMatrix { get; set; }
+
+        public Random RNG { get; private set; }
+        public Ship Player { get; private set; }
+        public InputManager InputManager { get; private set; }
+        public Game Game { get; private set; }
+
+        public static GameManager GetGameManager()
+        {
+            if(gameManager == null)
+                gameManager = new GameManager();
+            return gameManager;
+        }
+        public GameManager()
+        {
+            _gameObjects = new List<GameObject>();
+            _toBeRemoved = new List<GameObject>();
+            _toBeAdded = new List<GameObject>();
+            InputManager = new InputManager();
+            RNG = new Random();
+            WorldMatrix = Matrix.Identity;
+        }
+
+        public void Initialize(ContentManager content, Game game, Ship player)
+        {
+            Game = game;
+            _content = content;
+            Player = player;
+        }
+
+        public void Load(ContentManager content)
+        {
+            _teamColorEffect = content.Load<Effect>("TeamColors");
+            _fade = content.Load<Effect>("Fade");
+            foreach (GameObject gameObject in _gameObjects)
+            {
+                gameObject.Load(content);
+            }
+        }
+
+        public void HandleInput(InputManager inputManager)
+        {
+            foreach (GameObject gameObject in _gameObjects)
+            {
+                gameObject.HandleInput(this.InputManager);
+            }
+        }
+
+        public void CheckCollision()
+        {
+            // Checks once for every pair of 2 GameObjects if the collide.
+            for (int i = 0; i < _gameObjects.Count; i++)
+            {
+                for (int j = i+1; j < _gameObjects.Count; j++)
+                {
+                    if (_gameObjects[i].CheckCollision(_gameObjects[j]))
+                    {
+                        _gameObjects[i].OnCollision(_gameObjects[j]);
+                        _gameObjects[j].OnCollision(_gameObjects[i]);
+                    }
+                }
+            }
+            
+        }
+        
+        public void Update(GameTime gameTime) 
+        {
+            InputManager.Update();
+
+            // Handle input
+            HandleInput(InputManager);
+
+
+            // Update
+            foreach (GameObject gameObject in _gameObjects)
+            {
+                gameObject.Update(gameTime);
+            }
+
+            // Check Collission
+            CheckCollision();
+
+            foreach (GameObject gameObject in _toBeAdded)
+            {
+                gameObject.Load(_content);
+                _gameObjects.Add(gameObject);
+            }
+            _toBeAdded.Clear();
+
+            foreach (GameObject gameObject in _toBeRemoved)
+            {
+                gameObject.Destroy();
+                _gameObjects.Remove(gameObject);
+            }
+            _toBeRemoved.Clear();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) 
+        {
+            Viewport viewport = Game.GraphicsDevice.Viewport;
+            RenderTarget2D intermediateTarget = new RenderTarget2D(Game.GraphicsDevice, viewport.Width, viewport.Height);
+            
+            Game.GraphicsDevice.SetRenderTarget(intermediateTarget);
+
+            spriteBatch.Begin(effect: _teamColorEffect);
+            foreach (GameObject gameObject in _gameObjects)
+            {
+                gameObject.Draw(gameTime, spriteBatch);
+            }
+            spriteBatch.End();
+
+            Game.GraphicsDevice.SetRenderTarget(null);
+            _fade.Parameters["Dimensions"]?.SetValue(new Vector2(intermediateTarget.Width, intermediateTarget.Height));
+            _fade.Parameters["Radius"]?.SetValue(0.1f);
+            spriteBatch.Begin(effect:_fade);
+            Color fadeColor = Color.Red;
+            fadeColor.A = (byte)((Math.Cos(gameTime.TotalGameTime.TotalSeconds)+1)/2*255);
+            spriteBatch.Draw(intermediateTarget, new Vector2(0, 0), fadeColor);
+            spriteBatch.End();
+
+            intermediateTarget.Dispose();
+        }
+
+        /// <summary>
+        /// Add a new GameObject to the GameManager. 
+        /// The GameObject will be added at the start of the next Update step. 
+        /// Once it is added, the GameManager will ensure all steps of the game loop will be called on the object automatically. 
+        /// </summary>
+        /// <param name="gameObject"> The GameObject to add. </param>
+        public void AddGameObject(GameObject gameObject)
+        {
+            _toBeAdded.Add(gameObject);
+        }
+
+        /// <summary>
+        /// Remove GameObject from the GameManager. 
+        /// The GameObject will be removed at the start of the next Update step and its Destroy() mehtod will be called.
+        /// After that the object will no longer receive any updates.
+        /// </summary>
+        /// <param name="gameObject"> The GameObject to Remove. </param>
+        public void RemoveGameObject(GameObject gameObject)
+        {
+            _toBeRemoved.Add(gameObject);
+        }
+
+        /// <summary>
+        /// Get a random location on the screen.
+        /// </summary>
+        public Vector2 RandomScreenLocation()
+        {
+            return new Vector2(
+                RNG.Next(0, Game.GraphicsDevice.Viewport.Width),
+                RNG.Next(0, Game.GraphicsDevice.Viewport.Height));
+        }
+    }
+}
